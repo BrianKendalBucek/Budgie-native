003f4b01e6b70a1c4e691cbc8fb2d2cf01c3dba5
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-02-17 19:40:42 -0600

    switched to Material UI for tab bar

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,13 +1,16 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Icons from '@react-navigation/material-bottom-tabs'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 // Screens
 import StatisticsScreen from './screens/StatisticsScreen'
 import CategoriesScreen from './screens/CategoriesScreen'
 import ExpendituresScreen from './screens/ExpendituresScreen'
 import ConverterScreen from './screens/ConverterScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 // Screen names
 const statisticsName = 'Statistics';
@@ -15,52 +18,57 @@ const categoriesName = 'Categories';
 const expendituresName = 'Expenditures';
 const converterName = 'Converter';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function MainContainer() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName={statisticsName}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            let rn = route.name;
-
-            if (rn === statisticsName) {
-              iconName = focused ? 'md-stats-chart' : 'md-stats-chart-outline'
-            } else if (rn === categoriesName) {
-              iconName = focused ? 'md-grid' : 'md-grid-outline'
-            } else if (rn === expendituresName) {
-              iconName = focused ? 'wallet' : 'wallet-outline';
-            } else if (rn === converterName) {
-              iconName = focused ? 'sync-circle' : 'sync-circle-outline';
-            }
-
-            return <Ionicons 
-              name={iconName} 
-              size={size} 
-              color={'grey'}
-              />
-          },
-        })}
-        // screenOptions={{
-        //   activeTintcolor: 'tomato',
-        //   inactiveTintcolor: 'grey',
-        //   labelStyle: { paddingBottom: 10, fontSize: 10 },
-        //   style: { padding: 10, height: 70 }
-        // }}
-
-        >
-
+      <Tab.Navigator>
         <Tab.Screen name={statisticsName} component={StatisticsScreen} />
         <Tab.Screen name={categoriesName} component={CategoriesScreen} />
         <Tab.Screen name={expendituresName} component={ExpendituresScreen} />
         <Tab.Screen name={converterName} component={ConverterScreen} />
-
-
       </Tab.Navigator>
-
-    </NavigationContainer >
+    </NavigationContainer>
   );
 }
+    // <NavigationContainer>
+    //   <Tab.Navigator
+    //     initialRouteName={statisticsName}
+    //     screenOptions={({ route }) => ({
+    //       tabBarIcon: ({ focused, color, size }) => {
+    //         let iconName;
+    //         let rn = route.name;
+
+    //         if (rn === statisticsName) {
+    //           iconName = focused ? 'md-stats-chart' : 'md-stats-chart-outline'
+    //         } else if (rn === categoriesName) {
+    //           iconName = focused ? 'md-grid' : 'md-grid-outline'
+    //         } else if (rn === expendituresName) {
+    //           iconName = focused ? 'wallet' : 'wallet-outline';
+    //         } else if (rn === converterName) {
+    //           iconName = focused ? 'sync-circle' : 'sync-circle-outline';
+    //         }
+
+    //         return <Ionicons
+    //           name={iconName}
+    //           size={size}
+    //           color={'grey'}
+    //           />
+    //       },
+    //     })}
+    //     // screenOptions={{
+    //     //   activeTintcolor: 'tomato',
+    //     //   inactiveTintcolor: 'grey',
+    //     //   labelStyle: { paddingBottom: 10, fontSize: 10 },
+    //     //   style: { padding: 10, height: 70 }
+    //     // }}
+
+    //     >
+
+    //
+
+
+    //   </Tab.Navigator>
+
+    // </NavigationContainer >
