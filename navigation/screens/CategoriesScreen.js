d6dc7455dcfdaab522ec8e9f8d88812a5ae6309e
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-02-18 10:29:51 -0600

    Categories structure built with material ui

--- navigation/screens/CategoriesScreen.js
+++ navigation/screens/CategoriesScreen.js
@@ -1,12 +1,48 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { TextInput, ListItem, Button } from "@react-native-material/core";
+import { AppBar, HStack, IconButton } from "@react-native-material/core";
 
-export default function CategoriesScreen({ navigation }) {
+
+export default function CategoriesScreen({ props }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text
-        onPress={() => navigation.nativate('Statistics')}
-        style={{ fontSize: 26, fontWeight: 'bold' }}>Categories Screen</Text>
+    <View
+    // style={{
+    //   flex: 1,
+    //   alignItems: 'center',
+    //   justifyContent: 'center'
+    // }}
+    >
+      <View style={{ flexDirection: 'row', width: window.width, margin: 10, padding: 4, alignItems: 'center', justifyContent: 'center', borderWidth: 4, borderColor: 'lightgrey', borderRadius: 10, backgroundColor: '#fff' }}>
+        <View style={{ flex: 4 }}>
+          <TextInput
+            // onChangeText={(textEntry) => { this.setState({ searchText: textEntry }) }}
+            style={{ backgroundColor: 'transparent', variant: 'outlined' }}
+            label="Add New Category"
+          // onSubmitEditing={() => { this.onSubmit(this.state.searchText) }}
+          />
+        </View>
+        <View style={{ flex: 1 }}>
+          <Button
+            style={{ width: 70, margin: 5, backgroundColor: "lightblue"}}
+            title="Add"
+            // onPress={
+            //   () => this.onSubmit(this.state.searchText)
+            // }
+            >
+            <IconButton
+              icon={props => <MaterialCommunityIcons name="logout" {...props} />}
+              {...props}
+            />
+          </Button>
+        </View>
+      </View>
+      <ListItem title="Groceries" />
+      <ListItem title="Rent" />
+      <ListItem title="Entertainment" />
+      <ListItem title="Utilities" />
+
+
     </View>
   )
 }
