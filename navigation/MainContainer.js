1de40391a852df3baba3d3b28b975ee4dffd41c2
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-02-18 09:19:11 -0600

    Header now present

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -2,7 +2,11 @@ import * as React from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { AppBar, HStack, IconButton } from "@react-native-material/core";
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
 
 // Screens
 import StatisticsScreen from './screens/StatisticsScreen'
@@ -21,94 +25,84 @@ const Tab = createMaterialBottomTabNavigator();
 
 export default function MainContainer() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Feed"
-        activeColor="#1976d2"
-        barStyle={{ backgroundColor: 'lightblue' }}
-    
-      >
-        <Tab.Screen
-          name={statisticsName}
-          component={StatisticsScreen}
-          options={{
-            tabBarLabel: 'Statistics',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="chart-box" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name={categoriesName}
-          component={CategoriesScreen}
-          options={{
-            tabBarLabel: 'Categories',
-            tabBarIcon: ({ color }) => (
-              <MaterialIcons name="category" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name={expendituresName}
-          component={ExpendituresScreen}
-          options={{
-            tabBarLabel: 'Expenditures',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="wallet" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name={converterName}
-          component={ConverterScreen}
-          options={{
-            tabBarLabel: 'Converter',
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="sync-circle" color={color} size={26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <>
+      <AppBar
+      style={styles.header}
+      title="Budgie"
+      leading={props => (
+        <IconButton icon={props => <Icon name="menu" {...props} />} {...props} />
+      )}
+      trailing={props => (
+        <HStack>
+          <IconButton
+            icon={props => <Icon name="magnify" {...props} />}
+            {...props}
+          />
+          <IconButton
+            icon={props => <Icon name="dots-vertical" {...props} />}
+            {...props}
+          />
+        </HStack>
+      )}
+    />
+      
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Feed"
+          activeColor="#1976d2"
+          barStyle={{ backgroundColor: 'lightblue' }}
+
+        >
+          <Tab.Screen
+            name={statisticsName}
+            component={StatisticsScreen}
+            options={{
+              tabBarLabel: 'Statistics',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="chart-box" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name={categoriesName}
+            component={CategoriesScreen}
+            options={{
+              tabBarLabel: 'Categories',
+              tabBarIcon: ({ color }) => (
+                <MaterialIcons name="category" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name={expendituresName}
+            component={ExpendituresScreen}
+            options={{
+              tabBarLabel: 'Expenditures',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="wallet" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name={converterName}
+            component={ConverterScreen}
+            options={{
+              tabBarLabel: 'Converter',
+              tabBarIcon: ({ color }) => (
+                <Ionicons name="sync-circle" color={color} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-    // <NavigationContainer>
-    //   <Tab.Navigator
-    //     initialRouteName={statisticsName}
-    //     screenOptions={({ route }) => ({
-    //       tabBarIcon: ({ focused, color, size }) => {
-    //         let iconName;
-    //         let rn = route.name;
 
-    //         if (rn === statisticsName) {
-    //           iconName = focused ? 'md-stats-chart' : 'md-stats-chart-outline'
-    //         } else if (rn === categoriesName) {
-    //           iconName = focused ? 'md-grid' : 'md-grid-outline'
-    //         } else if (rn === expendituresName) {
-    //           iconName = focused ? 'wallet' : 'wallet-outline';
-    //         } else if (rn === converterName) {
-    //           iconName = focused ? 'sync-circle' : 'sync-circle-outline';
-    //         }
-
-    //         return <Ionicons
-    //           name={iconName}
-    //           size={size}
-    //           color={'grey'}
-    //           />
-    //       },
-    //     })}
-    //     // screenOptions={{
-    //     //   activeTintcolor: 'tomato',
-    //     //   inactiveTintcolor: 'grey',
-    //     //   labelStyle: { paddingBottom: 10, fontSize: 10 },
-    //     //   style: { padding: 10, height: 70 }
-    //     // }}
-
-    //     >
-
-    //
-
-
-    //   </Tab.Navigator>
-
-    // </NavigationContainer >
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "red",
+    height: 100,
+    paddingTop: 40
+  }
+})
