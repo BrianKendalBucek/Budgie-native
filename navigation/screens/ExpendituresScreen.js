4f768f71cc5cb52ee27e2267c1edbd595760fccc
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-02-18 15:39:43 -0600

    added date picker

--- navigation/screens/ExpendituresScreen.js
+++ navigation/screens/ExpendituresScreen.js
@@ -1,12 +1,47 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, StyleSheet, Text, Platform } from 'react-native';
+import { Box, Container, Tab, Tabs, TextInput, Autocomplete } from "@react-native-material/core";
+// import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
-export default function ExpendituresScreen({ navigation }) {
+export default function ExpendituresScreen({ props }) {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    hideDatePicker();
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text
-        onPress={() => navigation.nativate('Statistics')}
-        style={{ fontSize: 26, fontWeight: 'bold' }}>Expenditures Screen</Text>
+    <View>
+      <TextInput
+        // onChangeText={(textEntry) => { this.setState({ searchText: textEntry }) }}
+        style={{ backgroundColor: 'transparent', variant: 'outlined' }}
+        label="Price of item"
+      // onSubmitEditing={() => { this.onSubmit(this.state.searchText) }}
+      />
+      {/* <Autocomplete /> */}
+      <Button title="Show Date Picker" onPress={showDatePicker} />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
+      <TextInput
+        // onChangeText={(textEntry) => { this.setState({ searchText: textEntry }) }}
+        style={{ backgroundColor: 'transparent', variant: 'outlined' }}
+        label="Title"
+      // onSubmitEditing={() => { this.onSubmit(this.state.searchText) }}
+      />
     </View>
   )
 }
